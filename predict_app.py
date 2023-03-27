9a92adfe8ad53a61d14fa0e6c21fa92c467d3a00
lourencofelipe <felipe.pucsc@gmail.com>
2023-03-27 15:55:23 +1300

    Test app

--- predict_app.py
+++ predict_app.py
@@ -1,97 +1,102 @@
 from flask import Flask, request, jsonify, render_template
+import keras.models
 from keras.preprocessing.image import img_to_array
-#from keras.models import Sequential
+from keras.models import Sequential
 from keras.models import load_model
-#from keras import backend as K
+from keras import backend as K
 from flask_cors import CORS
-#import tensorflow as tf
+import tensorflow as tf
 from PIL import Image
-#import keras.models
 import numpy as np
 import base64
-#import sys
+import sys
 import io
-#import re 
+import re 
 import os
 
-# Initiate Flask.
+# Inicia o app flask.
 app = Flask(__name__)
 
 cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
-def loadModel():
+def carregarModelo():
     global model
     
-    # Load the trained model.
+    # Carrega o arquivo do modelo treinado.
     model = load_model('models/Modelo_Duas_Classes_98.h5')
     
     return model
 
-def processImage(image, expectedSize):
+def processarImagem(imagem, tamanhoEsperado):
     '''
-    Process the image inserted by the interface. Checks if the image is RGB, resizes it to the expected size and converts it to an array.
+        Processa imagem inserida por meio da interface. Verifica se a imagem está em RGB, 
+        redimensiona a imagem para o tamanho esperado e convete imagem para array.
     INPUT
-        IMAGE: Inserted image.
-        EXPECTEDSIZE: Final size that the model expects.
-    OUTPUT:
-        Returns the processed image.
+        IMAGEM: Imagem inserida.
+        TAMANHOESPERADO: Tamanho final que o modelo espera.
+    OUTPUT   
+        Retorna imagem processada.
     '''
-    
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    image = image.resize(expectedSize)
-    image = img_to_array(image)
-    image = image.astype('float32') 
-    image /= 255
+    if imagem.mode != "RGB":
+        imagem = imagem.convert("RGB")
+    imagem = imagem.resize(tamanhoEsperado)
+    imagem = img_to_array(imagem)
+    imagem = imagem.astype('float32') 
+    imagem /= 255
 
-    return image
+    return imagem
 
-loadModel()
+carregarModelo()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
-@app.route("/information", methods=['GET', 'POST'])
-def information():
+@app.route("/informations", methods=['GET', 'POST'])
+def informacoes():
     return render_template("informacoes.html")
 
-# Makes a request to the prediction model.
+# Criação de endpoint que requisita o modelo para predição.
 @app.route('/predict', methods = ['GET','POST'])
 def predict():
     '''
-    Makes the prediction process for the inserted image.
-    INPUT:
-        MESSAGE: Recieves the Json from the request.
-        ENCODEDIMAGE: Recieves the value associated with the key 'image' of Json alocated in the variable
-        'message'. The value is the image recieved by the client side in base64 format. 
-        DECODEDIMAGE: Makes a b64decode of the image.
-        PROCESSEDIMAGE: Recieves an instance of PIL dependence to open an image wich is alocated in memory in base64 format and than, keep the image in a bytes format.
-        PREDICTION: Recieves the predict method, passing the processed image in numpyArray and, calls the method 'tolist()' to convert it in a list.
-    OUTPUT: 
-        Dictionary which will be sent to the client side. Contains the keys responsible for prediction with the values that the model returns.
+        Realiza o processo de predição para a imagem inserida.
+    INPUT
+        MESSAGE: Recebe o json da requisição. 
+        IMAGEMCODIFICADA: Recebe o valor associado a chave 'image' do JSON alocado na variável 'message'. 
+        O valor é a imagem recebida via client no formato base64.
+        IMAGEMDECODIFICADA: Decodifica a imagem incialmente da base64
+        IMAGEM: Recebe instância da dependência PIL para abrir uma imagem que está em memória na base64 e então
+        manter com formato em bytes.
+        IMAGEMPROCESSADA: Recebe a imagem processada no tamanho utilizado pelo modelo.
+        PREDICAO: Recebe o método predict, recebendo a imagem processada em numpyArray, então deve-se instanciar
+        método tolist() para converter em uma lista;
+        RESPONSE: Dicionário que será enviado de volta para o client. Contém as chaves responsáveis pela predição 
+        com os valores retornados pelo modelo.
+    OUTPUT   
+        Retorna json da resposta.
     '''
     message = request.get_json(force = True)
-    encodedImage = message['image']
-    decodedImage = base64.b64decode(encodedImage)
-    image = Image.open(io.BytesIO(decodedImage))
-    processedImage = processImage(image, expectedSize=(256, 239))
+    imagemCodificada = message['image']
+    imagemDecodificada = base64.b64decode(imagemCodificada)
+    imagem = Image.open(io.BytesIO(imagemDecodificada))
+    imagemProcessada = processarImagem(imagem, tamanhoEsperado=(256, 239))
     
-    prediction = model.predict(np.expand_dims(processedImage, axis = 0))
+    predicao = model.predict(np.expand_dims(imagemProcessada, axis = 0))
 
-    # Class of the analyzed image.
-    labelAnalyzedImage = model.predict_classes(np.expand_dims(processedImage, axis = 0)).tolist()
+    # Classe da imagem analisada.
+    rotuloImagemAnalisada = model.predict_classes(np.expand_dims(imagemProcessada, axis = 0)).tolist()
     
-    # Predictions for the images classes.
-    positivePrediction = (prediction[:,1] * 100).tolist() 
-    negativePrediction = (prediction[:,0] * 100).tolist()
+    # Predições para as classes de imagens.
+    predicaoPositiva = (predicao[:,1] * 100).tolist() 
+    predicaoNegativa = (predicao[:,0] * 100).tolist()
 
-   # Sends the formatted response to the client side.
+   # Envia a resposta em json para o client.
     response = {
         'predicao': {
-            'sintomatica': positivePrediction,
-            'assintomatica': negativePrediction,
-            'classeImagem': labelAnalyzedImage
+            'sintomatica': predicaoPositiva,
+            'assintomatica': predicaoNegativa,
+            'classeImagem': rotuloImagemAnalisada
         }
     }
     
